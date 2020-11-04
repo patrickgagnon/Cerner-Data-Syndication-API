ce7b7a76c6df90e805dcda4e64514bdb2606c019
patrickgagnon <plgagnon00@gmail.com>
2020-11-04 12:26:44 -0500

    Download Deliveries throwing errors.

--- syndication-api.py
+++ syndication-api.py
@@ -17,12 +17,17 @@ class Syndication(object):
         self.channel_id = None
         self.feed_id = None
 
-    def __send_request__(self, endpoint, method = None, headers = None, params=None):
+    def __send_request__(self, endpoint, method = None, json=None, headers = None, params=None):
         if method == 'GET':
             return requests.get('{uri}{endpoint}'.format(uri=self.uri, endpoint=endpoint), headers=headers,
-                                params=params).json()
+                                params=params)#.json()
         else:
-            return requests.post('{uri}{endpoint}'.format(uri=self.uri, endpoint=endpoint), headers=headers)
+            if json:
+                return requests.post('{uri}{endpoint}'.format(uri=self.uri, endpoint=endpoint), headers=headers,
+                                     json=json)
+            else:
+                return requests.post('{uri}{endpoint}'.format(uri=self.uri, endpoint=endpoint), headers=headers)
+
 
     def get_feeds(self):
         return self.__send_request__(endpoint='/feeds', method='GET', headers=self.headers)
@@ -37,15 +42,27 @@ class Syndication(object):
         return self.__send_request__(endpoint='/channels/{channel_id}/deliveries'.format(channel_id=channel_id), method='GET', headers=self.headers)
 
     def get_channel_downloads(self, delivery_id=None, destination_path=None):
-        return self.__send_request__(endpoint='/downloads/{delivery_id}'.format(delivery_id=delivery_id), method='GET', headers=self.headers)
-        #cd=os.getcwd()
-        #temp=tarfile.open(name=destination_path, mode='r:gz')
-        # if downloads.status_code == 200:
-        #    with open(path, 'wb') as f:
-        #        f.write(downloads.content)
+        response=self.__send_request__(endpoint='/downloads/{delivery_id}'.format(delivery_id=delivery_id), method='GET', headers=self.headers)
+        wd=os.getcwd()
+        path= destination_path+dt.datetime.strftime(dt.datetime.today(), '%Y-%m-%d') +'.tar'
+
+
+        with open(path, 'wb') as i:
+            i.write(response.content)
+            i.close()
+
+        temp=tarfile.open(name=path, mode='r:gz')
+        os.chdir(destination_path)
+        temp.extractall()
+        temp.close()
+        os.chdir(wd)
+
+
+
 
 if __name__ == '__main__':
     s = Syndication()
-    #print(s.get_channels())
-    #print(s.get_channel_status(channel_id='4b1d6d81-2067-41d9-bb19-f9e1f68da036'))
-    #print(s.get_channel_status(channel_id='83bec28a-b367-41c2-b468-7d8d6dd92f0c'))
+    s.get_channel_downloads('f92df8bc-021f-4f0e-b107-5edb89be5690', destination_path= 'C:/Users/PatrickGagnon/Documents/Python-Projects/Cerner-Data-Syndication-API/Test_Files/')
+    #print(foo)
+    #print(s.get_channels()
+    #print(s.get_channel_deliveries(channel_id='83bec28a-b367-41c2-b468-7d8d6dd92f0c'))
