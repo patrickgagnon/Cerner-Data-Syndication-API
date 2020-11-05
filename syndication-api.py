807ec613ef94f326ad63b46ee6a410df09ab17c9
patrickgagnon <plgagnon00@gmail.com>
2020-11-05 16:21:12 -0500

    Still something is wrong T-T

--- syndication-api.py
+++ syndication-api.py
@@ -1,4 +1,3 @@
-
 if __name__ == '__main__':
     from settings import *
 else:
@@ -7,6 +6,7 @@ import requests
 import tarfile
 import datetime as dt
 import os
+
 
 class Syndication(object):
 
@@ -17,10 +17,10 @@ class Syndication(object):
         self.channel_id = None
         self.feed_id = None
 
-    def __send_request__(self, endpoint, method = None, json=None, headers = None, params=None):
+    def __send_request__(self, endpoint, method = None, json = None, headers = None, params = None):
         if method == 'GET':
             return requests.get('{uri}{endpoint}'.format(uri=self.uri, endpoint=endpoint), headers=headers,
-                                params=params)#.json()
+                                params=params).json()
         else:
             if json:
                 return requests.post('{uri}{endpoint}'.format(uri=self.uri, endpoint=endpoint), headers=headers,
@@ -28,41 +28,44 @@ class Syndication(object):
             else:
                 return requests.post('{uri}{endpoint}'.format(uri=self.uri, endpoint=endpoint), headers=headers)
 
-
     def get_feeds(self):
         return self.__send_request__(endpoint='/feeds', method='GET', headers=self.headers)
 
     def get_channels(self):
-        return self.__send_request__(endpoint='/channels', method='GET',headers=self.headers)
+        return self.__send_request__(endpoint='/channels', method='GET', headers=self.headers)
 
-    def get_channel_status(self, channel_id=None):
-        return self.__send_request__(endpoint='/channels/{channel_id}'.format(channel_id=channel_id), method='GET',headers=self.headers)
+    def get_channel_status(self, channel_id = None):
+        return self.__send_request__(endpoint='/channels/{channel_id}'.format(channel_id=channel_id), method='GET',
+                                     headers=self.headers)
 
-    def get_channel_deliveries(self, channel_id=None):
-        return self.__send_request__(endpoint='/channels/{channel_id}/deliveries'.format(channel_id=channel_id), method='GET', headers=self.headers)
+    def get_channel_deliveries(self, channel_id = None):
+        return self.__send_request__(endpoint='/channels/{channel_id}/deliveries'.format(channel_id=channel_id),
+                                     method='GET', headers=self.headers)
 
-    def get_channel_downloads(self, delivery_id=None, destination_path=None):
-        response = self.__send_request__(endpoint='/downloads/{delivery_id}'.format(delivery_id=delivery_id), method='GET', headers=self.headers)
-        wd=os.getcwd()
-        path= destination_path + dt.datetime.strftime(dt.datetime.today(), '%Y-%m-%d') + '.tar.gz'
-
+    def get_channel_downloads(self, delivery_id = None, destination_path = None):
+        response = self.__send_request__(endpoint='/downloads/{delivery_id}'.format(delivery_id=delivery_id),
+                                         method='GET', headers=self.headers)
+        wd = os.getcwd()
+        path = destination_path + dt.datetime.strftime(dt.datetime.today(), '%Y-%m-%d') + '.tar'
 
         with open(path, 'wb') as i:
             i.write(response)
+
         i.close()
 
-        temp=tarfile.open(name=path, mode='r:gz')
+        temp = tarfile.open(name=path, mode='r:gz')
         os.chdir(destination_path)
         temp.extractall()
         temp.close()
         os.chdir(wd)
 
-
+    def get_channel_downloads2(self, delivery_id = None, destination_path = None):
+        return self.__send_request__(endpoint='/downloads/{delivery_id}'.format(delivery_id=delivery_id), method='GET',headers=self.headers)
 
 
 if __name__ == '__main__':
     s = Syndication()
-    s.get_channel_downloads('f92df8bc-021f-4f0e-b107-5edb89be5690', destination_path= 'C:/Users/PatrickGagnon/Documents/Python-Projects/Cerner-Data-Syndication-API/Test_Files/')
-    #print(foo)
-    #print(s.get_channels()
+    s.get_channel_downloads('217a4ee0-bb87-4877-a017-83880b5ee399',destination_path=TEST_PATH)
+
+    # print(s.get_channels())
     #print(s.get_channel_deliveries(channel_id='83bec28a-b367-41c2-b468-7d8d6dd92f0c'))
